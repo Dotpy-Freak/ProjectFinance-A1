713a39598ed84881ee8ce3f0d3bddd3fa9ad07cd
Aman Karki <sgtest.one@gmail.com>
2025-05-16 11:08:58 -0300

    Modified all classes and removed dummy values

--- src/test/java/com/example/UserTest.java
+++ src/test/java/com/example/UserTest.java
@@ -1,25 +1,39 @@
 package com.example;
 import com.example.models.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import javax.annotation.processing.Generated;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
     User user;
+    @BeforeAll
+    public static void printInit(){
+        System.out.println("Initializing and starting test");
+    }
+
     @BeforeEach
     public void setup(){
          user = new User("Test");
     }
 
-    @Test
+    @Test @DisplayName("printing name test")
     public void printName(){
         assertEquals("Test",user.getName());
     }
 
+    @DisplayName("cleaning test")
     @AfterEach
     public void cleanUp(){
         user = null;
     }
+
+    @AfterAll
+    public static void finishedTest(){
+        System.out.println("Finished all tests");
+    }
+
+
 
 }
